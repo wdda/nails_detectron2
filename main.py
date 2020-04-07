aaccc8a01ae6ce87d44db3ab1c981894c3a01241
wdda <dima@wdda.pro>
2020-04-07 03:31:58 +0300

    Dev

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import dataset
-from detectron2.data import DatasetCatalog
-
+from detectron2.data import DatasetCatalog, MetadataCatalog
+import random
+from detectron2.utils.visualizer import Visualizer
+import cv2
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+import cv2
+import torch
 
 def get_data():
     return dataset.DataSet('train').set()
 
 
 datasetForDetector = DatasetCatalog.register("nails_dataset", get_data)
+MetadataCatalog.get("nails_dataset").thing_classes = ["nail"]
+
+im = cv2.imread("images/train/668.jpg")
+
+# Create config
+cfg = get_cfg()
+cfg.merge_from_file("configs/COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"
+
+# Create predictor
+predictor = DefaultPredictor(cfg)
+
+# Make prediction
+outputs = predictor(im)
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imshow(v.get_image()[:, :, ::-1])
+
+
+# dataset_dicts = DatasetCatalog.get("nails_dataset")
+
